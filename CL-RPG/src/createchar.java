ed22417416a98a302a3a6bc9d81457c34c3c8e01
vincent <dj.emtec@gmail.com>
2011-08-22 21:31:56 +0200

    add skill.xml

--- CL-RPG/src/createchar.java
+++ CL-RPG/src/createchar.java
@@ -9,14 +9,16 @@ import javax.xml.parsers.DocumentBuilder;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException; 
 
-public class createchar {
+public class createchar{
 	
 	Scanner input = new Scanner(System.in);
 	String[] classes = null;
+	String[] classData;
 	int amountClasses;
 	
 	public createchar(){
-		getClassData();
+		getClassName();
+		
 		String name;
 		String userClass;
 		Integer classInt = 0;
@@ -52,9 +54,12 @@ public class createchar {
 			}
 		}
 		while(Bln == false);
+		
+		getUserStats(classInt);
+		
 	}
 	
-	public void getClassData(){
+	public void getClassName(){
 
 		try {
             DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
@@ -115,4 +120,103 @@ public class createchar {
 		//System.exit (0);
         
 	}
+	
+	public void getUserStats(int classIdChosen){
+
+		try {
+            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse (new File("src/resources/classdata.xml"));
+
+            doc.getDocumentElement ().normalize ();
+
+            NodeList listOfClasses = doc.getElementsByTagName("class");
+
+            String classID;
+            int classIDint;
+            String strength;
+            String stamina;
+            String inteligence;
+            String spirit;
+            String physresitance;
+            String magicresitance;
+
+            for(int s=0; s<listOfClasses.getLength() ; s++){
+
+
+                Node firstPersonNode = listOfClasses.item(s);
+                if(firstPersonNode.getNodeType() == Node.ELEMENT_NODE){
+
+
+                    Element firstClassElement = (Element)firstPersonNode;
+                    
+                    NodeList idList = firstClassElement.getElementsByTagName("id");
+                    Element idElement = (Element)idList.item(0);
+                    NodeList textIdList = idElement.getChildNodes();                    
+                    classID =((Node)textIdList.item(0)).getNodeValue().trim(); 
+                    
+                    classIDint = Integer.parseInt( classID ); 
+                    
+                    if(classIDint == classIdChosen){
+                    	NodeList strengthList = firstClassElement.getElementsByTagName("strength");
+                        Element strengthElement = (Element)strengthList.item(0);
+                        NodeList textStrengthList = strengthElement.getChildNodes();                    
+                        strength =((Node)textStrengthList.item(0)).getNodeValue().trim(); 
+                        
+                        NodeList staminaList = firstClassElement.getElementsByTagName("stamina");
+                        Element staminaElement = (Element)staminaList.item(0);
+                        NodeList textStaminaList = staminaElement.getChildNodes();                    
+                        stamina =((Node)textStaminaList.item(0)).getNodeValue().trim(); 
+                        
+                        NodeList inteligenceList = firstClassElement.getElementsByTagName("inteligence");
+                        Element inteligenceElement = (Element)inteligenceList.item(0);
+                        NodeList textInteligenceList = inteligenceElement.getChildNodes();                    
+                        inteligence =((Node)textInteligenceList.item(0)).getNodeValue().trim(); 
+                        
+                        NodeList spiritList = firstClassElement.getElementsByTagName("spirit");
+                        Element spiritElement = (Element)spiritList.item(0);
+                        NodeList textSpiritList = spiritElement.getChildNodes();                    
+                        spirit =((Node)textSpiritList.item(0)).getNodeValue().trim(); 
+                        
+                        NodeList physresitanceList = firstClassElement.getElementsByTagName("physresitance");
+                        Element physresitanceElement = (Element)physresitanceList.item(0);
+                        NodeList textPhysresitanceList = physresitanceElement.getChildNodes();                    
+                        physresitance =((Node)textPhysresitanceList.item(0)).getNodeValue().trim(); 
+                        
+                        NodeList magicresitanceList = firstClassElement.getElementsByTagName("magicresitance");
+                        Element magicresitanceElement = (Element)magicresitanceList.item(0);
+                        NodeList textMagicresitanceList = magicresitanceElement.getChildNodes();                    
+                        magicresitance =((Node)textMagicresitanceList.item(0)).getNodeValue().trim(); 
+
+
+                        classData = new String[]{strength, stamina, inteligence, spirit, physresitance, magicresitance};
+                        
+                        
+                    }
+
+
+                }//end of if clause
+
+                
+            }//end of for loop with s var
+            
+
+        }catch (SAXParseException err) {
+        System.out.println ("** Parsing error" + ", line " 
+             + err.getLineNumber () + ", uri " + err.getSystemId ());
+        System.out.println(" " + err.getMessage ());
+
+        }catch (SAXException e) {
+        Exception x = e.getException ();
+        ((x == null) ? e : x).printStackTrace ();
+
+        }catch (Throwable t) {
+        t.printStackTrace ();
+        }
+        
+		//System.exit (0);
+        
+	}
+	
+	
 }
