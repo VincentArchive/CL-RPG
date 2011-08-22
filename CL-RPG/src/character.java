f1a5f3b308567626dcde44f59d177c2381d61d3b
vincent <dj.emtec@gmail.com>
2011-08-22 23:03:57 +0200

    added main menu

--- CL-RPG/src/character.java
+++ CL-RPG/src/character.java
@@ -10,6 +10,7 @@ public class character {
 	static int physresitance;
 	static int magicresitance;
 	
+	
 	public  character(String[] userData){
 		name = userData[0];
 		classID = Integer.parseInt( userData[1] ); 
@@ -34,6 +35,9 @@ public class character {
 	}
 	
 	public static void showStats(){
+		for(int i=0;i < 10; i++){
+			System.out.println("");
+		}
 		System.out.println("Your stats are");
 		if(strength != 0){System.out.println("strength "+ strength); }
 		if(stamina != 0)System.out.println("stamina "+ stamina);
@@ -45,7 +49,49 @@ public class character {
 	}
 	
 	public static void showMenu(){
+		int optionInt = 0;
+		Scanner input = new Scanner(System.in);
+
+
+		boolean Bln = true;
+		do {
+			Bln = true;
+			System.out.println("What do you want to do?");
+			System.out.println("1. Show stats");
+			System.out.println("2. Show skills");
+			System.out.println("3. Fight monsters!");
+			String option = input.nextLine();
+			
+			// probeer om string Userclass om te zetten naar integer
+			try  
+			   {  
+				optionInt = Integer.parseInt( option );  
+			   }  
+			// als converteren niet lukt, geef error, zen Bln op false ( hierdoor wordt while opnieuw uitgevoert )
+			catch( Exception e)  
+			   {  
+				   System.out.println("Geen getal gekozen.");
+				   Bln = false;
+			   }
+			// als bij vorige stap geen error gebeurt ( Bln nog steeds true )
+			// kijk of classInt tussen 1 en amountClasses ligt, zo niet zet bln op false waardoor while opnieuw uitgevoert
+			if(Bln = true){
+				if(optionInt < 1 || optionInt > 3){
+					System.out.println("Gekozen getal ligt niet tussen 1 en 3.");
+					Bln = false;
+				}
+			}
+		}
+		while(Bln == false);
 		
+		if(optionInt == 1){
+			showStats();
+			showMenu();
+		}else if(optionInt == 2){
+			showMenu();
+		}else if(optionInt == 3){
+			showMenu();
+		}
 	}
 	
 	
