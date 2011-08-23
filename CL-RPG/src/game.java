7dcc107536f3f0dcca811721413484d47981239e
vincent <dj.emtec@gmail.com>
2011-08-23 14:41:46 +0200

    testing

--- CL-RPG/src/game.java
+++ CL-RPG/src/game.java
@@ -1,14 +1,47 @@
-// importing user input scanner
-//import java.util.Scanner;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class game {
-	
-	public static void main(String[] args) {
-		createchar createChar = new createchar();
-		
-		//createChar.askName();
-		createChar.askClass();
-		
-	}
-	
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+
+public class game extends JFrame {
+
+    public game() {
+        initUI();
+    }
+
+    public final void initUI() {
+
+       JPanel panel = new JPanel();
+       getContentPane().add(panel);
+
+       panel.setLayout(null);
+
+       JButton quitButton = new JButton("Quit");
+       quitButton.setBounds(50, 60, 80, 30);
+       quitButton.addActionListener(new ActionListener() {
+           public void actionPerformed(ActionEvent event) {
+               System.exit(0);
+          }
+       });
+
+       panel.add(quitButton);
+
+       setTitle("Quit button");
+       setSize(300, 200);
+       setLocationRelativeTo(null);
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                game ex = new game();
+                ex.setVisible(true);
+            }
+        });
+    }
 }
